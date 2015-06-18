8699359a17067493d07955562d004750f06337aa
David Gwilt <david.gwilt@essentia-analytics.com>
2015-06-18 18:22:59 +0100

    Added some more debugging statements

--- conf/libsasl2-dev.sh
+++ conf/libsasl2-dev.sh
@@ -2,3 +2,14 @@
 echo "BUILD_DIR: $BUILD_DIR" | indent | indent
 echo "CACHE_DIR: $CACHE_DIR" | indent | indent
 echo "LP_DIR:    $LP_DIR"    | indent | indent
+
+ls -al $BUILD_DIR
+echo
+ls -al $CACHE_DIR
+echo
+ls -al $LP_DIR
+echo
+
+export TEST_VAR="a_value_for_test_var"
+echo "Set TEST_VAR to: $TEST_VAR"
+

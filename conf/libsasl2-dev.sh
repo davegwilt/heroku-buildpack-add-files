876f1d83f43820756fb0b86343388343b14432f2
David Gwilt <david.gwilt@essentia-analytics.com>
2015-06-18 18:56:45 +0100

    Show contents of export

--- conf/libsasl2-dev.sh
+++ conf/libsasl2-dev.sh
@@ -3,14 +3,15 @@ echo "BUILD_DIR: $BUILD_DIR" | indent | indent
 echo "CACHE_DIR: $CACHE_DIR" | indent | indent
 echo "LP_DIR:    $LP_DIR"    | indent | indent
 
+TEST_VAR="a_value_for_test_var"
+export-env TEST_VAR "$TEST_VAR"
+echo "Set TEST_VAR to: $TEST_VAR" | indent | indent
+
 ls -al $BUILD_DIR
 echo
 ls -al $CACHE_DIR
 echo
 ls -al $LP_DIR
 echo
-
-TEST_VAR="a_value_for_test_var"
-export-env TEST_VAR "$TEST_VAR"
-echo "Set TEST_VAR to: $TEST_VAR" | indent | indent
+cat $EXPORT_PATH
 
